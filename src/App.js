0da2c406f3b0ba5c7a443e7eb48c77ccabec7a02
Martin Shinebaum <mshinebaum@gmail.com>
2020-01-29 11:27:35 +0000

    final version

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Welcome to your first Codecademy App</p>
         <a
           className="App-link"
           href="https://reactjs.org"
